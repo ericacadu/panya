ea9fc1d2f1b41697cb4d6cdcb12ee71396a6df43
Erica <erica@Ericade-iMac.local>
2021-07-28 12:11:21 +0800

    upload pages

---
+++ src/components/Pagination.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="text-center"
+    v-show="pages.total_pages > 1">
+    <ul class="pagination d-flex justify-content-center my-4">
+      <li class="page-item" :class="pages.has_pre ? '' : 'disabled'">
+        <a class="page-link" href="#"
+          @click.prevent="$emit('get-datas', pages.current_page - 1)">
+          <span>&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"
+        v-for="(item, key) in pages.total_pages" :key="key"
+        :class="item === pages.current_page ? 'active' : ''">
+        <a class="page-link" href="#"
+          @click.prevent="$emit('get-datas', item)">
+        {{ item }}</a>
+      </li>
+      <li class="page-item" :class="pages.has_next ? '' : 'disabled'">
+        <a class="page-link" href="#"
+          @click.prevent="$emit('get-datas', pages.current_page + 1)">
+          <span>&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['pages'],
+};
+</script>
---
+++ src/mixins/FadeInMix.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  methods: {
+    fadeInOnLoad() {
+      const all = document.querySelectorAll('.fade-out');
+      const targetPos = document.getElementById('target').offsetTop;
+      const { innerHeight } = window;
+      all.forEach((item) => {
+        if (innerHeight - item.offsetTop >= innerHeight - targetPos) {
+          item.classList.add('fade-in');
+        }
+      });
+    },
+    fadeInOnScroll() {
+      const all = document.querySelectorAll('.fade-out');
+      const targetPos = document.getElementById('target').offsetTop;
+      const { innerHeight } = window;
+      const windowY = window.scrollY;
+      all.forEach((item) => {
+        if (windowY + targetPos - item.offsetTop >= innerHeight - targetPos) {
+          item.classList.add('fade-in');
+        }
+      });
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.fadeInOnScroll);
+  },
+  unmounted() {
+    window.removeEventListener('scroll', this.fadeInOnScroll);
+  },
+};
+</script>
