6197a5395875df1196ff798ae32b40e686459c43
Erica <erica@EricadeMac-mini.local>
2022-05-04 14:58:22 +0800

    remove editorconfig, update depoly setting

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/ericacadu/panya.git master:gh-pages
+git push -f git@github.com:ericacadu/panya.git master:gh-pages
 cd -
